47ef5540fc91465d33270c5caf52d3ffaa886c05
ParthBedekar <parthbedekar62@gmail.com>
2026-03-16 14:48:02 +0530

    Setup Deployment

--- src/main/java/org/app/cia/api/AnalysisController.java
+++ src/main/java/org/app/cia/api/AnalysisController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.Paths;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class AnalysisController {
